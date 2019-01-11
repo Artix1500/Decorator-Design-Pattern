dcdda2729e36e0ff16f520276aacfcb33fb623a3
Artix1500 <artur.haczek23@gmail.com>
2019-01-11 18:17:34 +0100

    Version 1.0

--- Main.py
+++ Main.py
@@ -1,1 +1,105 @@
-print("Hello world!")
+import abc
+
+
+class Pizza(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def pizza_type(self):
+        pass
+
+    @abc.abstractmethod
+    def cost(self):
+        pass
+
+
+class ToppingsDecorator(Pizza):
+    @abc.abstractmethod
+    def pizza_type(self):
+        pass
+
+
+class MakePizza(Pizza):
+    def cost(self):
+        pass
+
+    def pizza_type(self):
+        pass
+
+
+# PIZZAS
+class Margherita(Pizza):
+    def __init__(self, pizza):
+        self.pizza = pizza
+
+    def cost(self):
+        return 12.90
+
+    def pizza_type(self):
+        print("Margherita", end='')
+
+
+class FarmHouse(Pizza):
+    def __init__(self, pizza):
+        self.pizza = pizza
+
+    def cost(self):
+        return 17.90
+
+    def pizza_type(self):
+        print("FarmHouse", end='')
+
+
+# TOPPINGS
+class FreshTomato(ToppingsDecorator):
+    def __init__(self, pizza):
+        self.pizza = pizza
+
+    def cost(self):
+        return self.pizza.cost() + 2
+
+    def pizza_type(self):
+        self.pizza.pizza_type()
+        print(" fresh tomato", end='')
+
+
+class ExtraCheese(ToppingsDecorator):
+    def __init__(self, pizza):
+        self.pizza = pizza
+
+    def cost(self):
+        return self.pizza.cost() + 2.5
+
+    def pizza_type(self):
+        self.pizza.pizza_type()
+        print(" extra cheese", end='')
+
+
+class Mushrooms(ToppingsDecorator):
+    def __init__(self, pizza):
+        self.pizza = pizza
+
+    def cost(self):
+        return self.pizza.cost() + 4
+
+    def pizza_type(self):
+        self.pizza.pizza_type()
+        print(" mushrooms", end='')
+
+
+
+
+def main():
+    margherita = Margherita(MakePizza())
+    margherita = FreshTomato(margherita)
+    margherita.pizza_type()
+    print("\nPrice: {}".format(margherita.cost()))
+
+    farm_house = FarmHouse(MakePizza())
+    farm_house = FreshTomato(farm_house)
+    farm_house = ExtraCheese(farm_house)
+    farm_house = Mushrooms(farm_house)
+    farm_house.pizza_type()
+    print("\nPrice: {}".format(farm_house.cost()))
+
+
+if __name__ == "__main__":
+    main()
